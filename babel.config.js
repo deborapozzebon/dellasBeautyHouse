c98f0d642356662d04e832e2d1159a793b1006a8
Debora <debora.pozzebon@gmail.com>
2020-12-23 18:50:39 -0300

    first

---
+++ babel.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "presets": ["next/babel"],
+    //"plugins": [["styled-component", {"ssr": true}]]
+    "plugins": ["babel-plugin-styled-components"]
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import React from 'react'
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <h1>Hello</h1>
+            </main>
+        </div>
+    )
+}
+
+export default Home
