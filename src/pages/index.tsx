78986484180ee1bf38f1bd8f9b6644e90ecd8a9f
Debora <debora.pozzebon@gmail.com>
2020-12-26 20:29:53 -0300

    static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,45 @@
-import Head from 'next/head'
 import React from 'react'
 
-const Home: React.FC = () => {
+// const Home: React.FC = ({ user }) => {
+//     return (
+//         <div>
+//             <img src={user.avatar_url} />
+//             {/* <Head>
+//                 <title>Create Next App</title>
+//                 <link rel="icon" href="/favicon.ico" />
+//             </Head>
+
+//             <main>
+//                 <h1>Hello</h1>
+//             </main> */}
+//         </div>
+//     )
+// }
+
+export default function Home({ user }): JSX.Element {
     return (
         <div>
-            <Head>
-                <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            <main>
-                <h1>Hello</h1>
-            </main>
+            <img
+                src={user.avatar_url}
+                alt={user.name}
+                width={200}
+                height={200}
+                style={{ borderRadius: '50%' }}
+            />
+            <h1>{user.name}</h1>
         </div>
     )
 }
 
-export default Home
+export const getStaticProps = async () => {
+    const response = await fetch('https://api.github.com/users/deborapozzebon')
+    const data = await response.json()
+
+    return {
+        props: {
+            user: data
+        }
+    }
+}
+
+// export default Home
